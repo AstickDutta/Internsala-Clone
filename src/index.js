1d06299bd6aad2de6d67c74773a7a46122fe044a
AstickDutta <astickdutta19@gmail.com>
2022-09-13 18:18:34 +0530

    commiting index.js changes and mongoose

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ const express = require('express')
 const route = require('../src/routes/router')
 const app = express()
 const mongoose = require('mongoose')
+const routes = require('../src/routes/router')         
 app.use(express.json())
 
 
-mongoose.connect("mongodb+srv://Astick_Dutta:AavSNrGfPyPswMGg@cluster0.laksbb0.mongodb.net/internProjectG35", {
+mongoose.connect("mongodb+srv://Astick_Dutta:AavSNrGfPyPswMGg@cluster0.laksbb0.mongodb.net/internProjectG35-DB", {
     useNewUrlParser: true
 })
     .then(() => console.log("MongoDb is connected"))
     .catch(err => console.log(err))
 
 
-app.use(route);
+app.use('/', route)
 
 
 app.listen(process.env.PORT || 3000, function () {
