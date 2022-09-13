c951bb38f0515690f05799a6d73b3d6371f424a4
Ashutosh Kumar <ashutoshk043@gmail.com>
2022-09-13 17:21:58 +0530

    commiting folder structure

---
+++ src/model/collegeModel.js
@@ -0,0 +1,1 @@
+// { name: { mandatory, unique, example iith}, fullName: {mandatory, example `Indian Institute of Technology, Hyderabad`}, logoLink: {mandatory}, isDeleted: {boolean, default: false} }
---
+++ src/model/internModel.js
@@ -0,0 +1,1 @@
+// { name: {mandatory}, email: {mandatory, valid email, unique}, mobile: {mandatory, valid mobile number, unique}, collegeId: {ObjectId, ref to college model, isDeleted: {boolean, default: false}}
