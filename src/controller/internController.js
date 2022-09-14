086c1a66a07fbb2aaefde7d6b210734d7ec1d3aa
Ashutosh Kumar <ashutoshk043@gmail.com>
2022-09-14 17:01:05 +0530

    commiting

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,15 +1,19 @@
 const internModel = require('../model/internModel')
 const collegeModel = require('../model/collegeModel')
 const validator = require("email-validator");
+var _ = require('lodash');
 
-var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/im;
 
 const createIntern = async (req, res) => {
-
+    try {
+        
     const { name, email, mobile, collegeName, isDeleted } = req.body
- 
+    if(_.isEmpty(req.body)){
+        return res.status(400).send({ status: false, messgae: "All fields are mendatory...." })
+    }
 
-     if(!name) {
+    if(!name) {
         return res.status(400).send({ status: false, messgae: "Name is mendatory.." })
     } else if (!email) {
         return res.status(400).send({ status: false, messgae: "email is mendatory.." })
@@ -29,7 +33,7 @@ const createIntern = async (req, res) => {
         let findAllIntern = await internModel.find({ $or: [{ email: email }, { name: name }, { mobile: mobile }] })
         if (findAllIntern.length > 0) return res.status(400).send({ status: false, message: "You are already Registered.." })
 
-        let findAllCollege = await collegeModel.find({ "fullName": { $regex: collegeName, "$options": "i" } })
+        let findAllCollege = await collegeModel.find({ "name": { $regex: collegeName, "$options": "i" } })
         if (findAllCollege.length < 1) return res.status(400).send({ status: false, message: "College not found" })
 
         collegeId = findAllCollege[0]._id
@@ -39,6 +43,9 @@ const createIntern = async (req, res) => {
         let createIntern = await internModel.create(data)
         res.status(201).send({ status: true, data: createIntern })
     }
+} catch (error) {
+    res.status(500).send({ status: false, error: error.message })     
+}
 
 }
 
