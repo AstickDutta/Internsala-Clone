6dfb7cf011e717722198e51eec6792a1b2e62f36
Ashutosh Kumar <ashutoshk043@gmail.com>
2022-09-14 17:34:37 +0530

    commiting changes

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,20 +1,41 @@
-const mongoose = require ("mongoose")
 const collegeModel = require("../model/collegeModel");
-const internModel = require("../model/internModel");
 
 
-const createCollege = async (req,res) => {
-    try{
-    let requestBody = req.body
-    let saveData = await collegeModel.create(requestBody);
-    return res.status(201).send({ status : true, data: saveData})
-    
-
-}catch (err){
-    console.log(err)
-    return res.status(500).send({status : false, meessage: err.meessage})
+const isvalidResquest = (requestBody) => {
+    return Object.keys(requestBody).length > 0
 }
+
+
+
+const createCollege = async (req, res) => {
+    try {
+        let requestBody = req.body
+        if (!isvalidResquest(requestBody)) return res.status(400).send({ satus: false, message: " please provid valid details in body section" })
+
+        let { name, fullName, logoLink } = requestBody
+
+        if (!name) return res.status(400).send({ status: false, message: "Oppss..!! name is Required" });
+        
+        if (!fullName) return res.status(400).send({ status: false, message: "Oppss..!! fullName is required" });
+        
+        if (!logoLink) return res.status(400).send({ status: false, message: "Oppss..!! logo link is required" });
+
+        let newName = await collegeModel.findOne({ name })
+        if (newName) return res.status(400).send({ status: false, message: "Oppss..!! name is already used" });
+
+
+        let saveData = await collegeModel.create(requestBody);
+        return res.status(201).send({ status: true, data: saveData })
+
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send({ status: false, meessage: err.meessage })
+    }
 }
+
+
+
 
 
 
