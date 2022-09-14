3677ac54cd6fb4eaf81ee6c50bbab18876b8070d
Ashutosh Kumar <ashutoshk043@gmail.com>
2022-09-14 12:04:31 +0530

    saving changes

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -3,6 +3,8 @@ const internModel = require('../model/internModel')
 
 const createIntern =  async (req, res) =>{
 
+    const {name,email,mobile,collegeName,isDeleted} = req.body
+    res.send({status:true, data:req.body})
 }
 
 
