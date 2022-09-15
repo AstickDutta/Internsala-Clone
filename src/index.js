59da6a820f37d7bef861ad7287b28f45a85c3a2d
AstickDutta <astickdutta19@gmail.com>
2022-09-16 00:12:37 +0530

    final Push for project 2

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ mongoose.connect("mongodb+srv://Astick_Dutta:AavSNrGfPyPswMGg@cluster0.laksbb0.m
 
 app.use('/', route)
 
+app.use((req, res, next) => {
+    res.status(404).send({status: false ,error: "Not found" });
+   })
+
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
